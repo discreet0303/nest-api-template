ea365137ccdc4a44e69f1a14363700b116110efa
mvp-JimChang <chehsuan.chang@worca.io>
2023-05-29 10:34:30 +0800

    add airbnb eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,8 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
@@ -21,5 +23,8 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'class-methods-use-this': 'off',
+    'import/prefer-default-export': 'off',
+    'max-classes-per-file': 'off',
   },
 };
